e280dfab7cfc2eed5956f2adff2d3a9da54e1ad2
Anmol Kothari <anmolkothari22@gmail.com>
2026-02-28 11:20:21 +0530

    prod bug fix

--- db.js
+++ db.js
@@ -1,13 +1,22 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'testdb',
-  password: process.env.PASSWORD,
-  port: process.env.PORT,
-});
+let pool;
+
+if (process.env.DATABASE_URL) {
+  // Production (Render)
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+  });
+} else {
+  // Local
+  pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'testdb',
+    password: process.env.PASSWORD,
+    port: process.env.DB_PORT,
+  });
+}
 
 module.exports = pool;
-
-
