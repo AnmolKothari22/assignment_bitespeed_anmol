4d282f8556ecd02f713c2bb7dab5173d92e89ad3
Anmol Kothari <anmolkothari22@gmail.com>
2026-02-28 08:46:50 +0530

    added condition for both phone and email not existing

--- send_res.js
+++ send_res.js
@@ -2,11 +2,14 @@ const pool = require('./db');
 
 
 async function send_res(result){
-   // console.log(result.rows);
-const ro=await pool.query(`SELECT user_Id from user_tester where id=$1 `,[(result.rows)[0].id]);
-//console.log(ro.rows);
+    console.log("----------0");
+    console.log(result.rows[0].id,typeof(result.rows[0].id));
+const ro=await pool.query(`SELECT user_id from users where id=$1 `,[result.rows[0].id]);
+console.log("----------1");
+console.log(ro.rows);
+console.log("----------2");
 const userId=ro.rows[0].user_id;
-const ans=await pool.query(`SELECT * from tester where tester.id in (select id from user_tester where user_id=$1);`,[userId]);
+const ans=await pool.query(`SELECT * from customers where customers.id in (select id from users where user_id=$1);`,[userId]);
 const answer=ans.rows;
 let email_array=[];
 let phoneNumber_array=[];
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.get('/',async (req,res)=>{
     try {
-        const result = await pool.query('SELECT * FROM tester;');
+        const result = await pool.query('SELECT * FROM customers;');
         res.status(200).json(result.rows);
     } catch (err) {
         console.error(err);
@@ -17,59 +17,87 @@ app.get('/',async (req,res)=>{
     }
 });
 
-
 app.post('/identify',async (req,res)=>{
     try{
 
         email=req.body.email;
+        
         phoneNumber=req.body.phoneNumber;
-       // const currentlyAdded =await pool.query(`INSERT INTO tester(email,phoneNumber) VALUES($1,$2)`,[email, phoneNumber]);
+       // const currentlyAdded =await pool.query(`INSERT INTO customers(email,phoneNumber) VALUES($1,$2)`,[email, phoneNumber]);
        // const currentlyAddedId=currentlyAdded.rowCount();
-        const result = await pool.query(`SELECT * FROM tester WHERE email = $1 AND phoneNumber = $2`,[email, phoneNumber]);
+        const result = await pool.query(`SELECT * FROM customers WHERE email = $1 AND phoneNumber = $2`,[email, phoneNumber]);
 
         if(result.rowCount ==0){
-            const result2 = await pool.query(`SELECT * FROM tester WHERE email = $1 OR phoneNumber = $2`,[email, phoneNumber]);
+            const result2 = await pool.query(`SELECT * FROM customers WHERE email = $1 OR phoneNumber = $2`,[email, phoneNumber]);
 
             if(result2.rowCount==0){
-                const currentlyAdded =await pool.query(`INSERT INTO tester(email,phoneNumber) VALUES($1,$2)`,[email, phoneNumber]);
-                const row_size=await pool.query(`select count(*) from tester`);
-                const new_userId=(row_size.rows)[0]+1;
-                await pool.query(`INSERT INTO user_tester(user_id) values($1);`,[ new_userId]);
+                //both email and phone number doesnt exist
+                console.log("hiii\n");
+                const currentlyAdded =await pool.query(`INSERT INTO customers(email,phoneNumber) VALUES($1,$2)`,[email, phoneNumber]);
+                const row_size=await pool.query(`select count(*) from customers`);
+                console.log(row_size);
+                const new_userId=Number(row_size.rows[0].count)+1;
+                console.log("hii22",new_userId);
+                await pool.query(`INSERT INTO users(user_id) values($1);`,[new_userId]);
+                console.log("1");
             }
             else{
-                //joining two users
-                const email_min=await pool.query(`select min(id) from tester where email=$1`,[email]);
+                console.log("joining two users");
+                const email_min=await pool.query(`select min(id) from customers where email=$1`,[email]);
                 const email_min_id=email_min.rows[0].min;
-                const phoneNumber_min=await pool.query(`select min(id) from tester where phoneNumber=$1`,[phoneNumber]);
+                const phoneNumber_min=await pool.query(`select min(id) from customers where phoneNumber=$1`,[phoneNumber]);
                 const phoneNumber_min_id=phoneNumber_min.rows[0].min;
-               // console.log(phoneNumber_min_id);
-                let user;
-                let sup_user;
-                if(email_min_id < phoneNumber_min_id){
-                    user=await pool.query(`select user_id from user_tester where id=$1 `,[phoneNumber_min_id]);
-                    sup_user=await pool.query(`select user_id from user_tester where id=$1 `,[email_min_id]);
+                console.log(phoneNumber_min_id);
+
+                if((phoneNumber_min_id!=null) && (email_min_id!=null)){
+                    let user;
+                    let sup_user;
+                    if((email_min_id < phoneNumber_min_id)){
+                        user=await pool.query(`select user_id from users where id=$1 `,[phoneNumber_min_id]);
+                        sup_user=await pool.query(`select user_id from users where id=$1 `,[email_min_id]);
+                    }
+                    else{
+                        user=await pool.query(`select user_id from users where id=$1 `,[email_min_id]);
+                        sup_user=await pool.query(`select user_id from users where id=$1 `,[phoneNumber_min_id]);
+                    }
+                    console.log("got the user ",user);
+                    const sup_user_id=(sup_user.rows)[0].user_id;
+                    const user_id=(user.rows)[0].user_id;
+                    console.log(user_id,sup_user_id);
+                    await pool.query(`UPDATE customers SET updatedat=now() WHERE customers.id in (select id from users where user_id=$1)`,[user_id]);
+                    await pool.query(`UPDATE users SET user_id=$1 WHERE user_id=$2;`,[sup_user_id,user_id]);
+
+                    await pool.query(`INSERT INTO customers(email,phoneNumber) VALUES($1,$2)`,[email, phoneNumber]);
+                    await pool.query(`INSERT INTO users(user_id) values($1);`,[sup_user_id]);
                 }
                 else{
-                    user=await pool.query(`select user_id from user_tester where id=$1 `,[email_min_id]);
-                    sup_user=await pool.query(`select user_id from user_tester where id=$1 `,[phoneNumber_min_id]);
+                    if(phoneNumber_min_id==null){
+                        let user=await pool.query(`select user_id from users where id=$1 `,[email_min_id]);
+                        const user_id=(user.rows)[0].user_id;
+                        await pool.query(`INSERT INTO customers(email,phoneNumber) VALUES($1,$2)`,[email, phoneNumber]);
+                        await pool.query(`INSERT INTO users(user_id) values($1);`,[user_id]);
+                    }
+                    else{
+                        let user=await pool.query(`select user_id from users where id=$1 `,[phoneNumber_min_id]);
+                        const user_id=(user.rows)[0].user_id;
+                        await pool.query(`INSERT INTO customers(email,phoneNumber) VALUES($1,$2)`,[email, phoneNumber]);
+                        await pool.query(`INSERT INTO users(user_id) values($1);`,[user_id]);
+                    }
                 }
-                console.log(user);
-                const sup_user_id=(sup_user.rows)[0].user_id;
-                const user_id=(user.rows)[0].user_id;
-                console.log(user_id,sup_user_id);
-                await pool.query(`UPDATE user_tester SET user_id=$1 WHERE user_id=$2;`,[sup_user_id,user_id]);
-                await pool.query(`INSERT INTO tester(email,phoneNumber) VALUES($1,$2)`,[email, phoneNumber]);
-                await pool.query(`INSERT INTO user_tester(user_id) values($1);`,[sup_user_id]);
             }
 
-            const result3 = await pool.query(`SELECT * FROM tester WHERE email = $1 AND phoneNumber = $2;`,[email, phoneNumber]);
+
+            const result3 = await pool.query(`SELECT * FROM customers WHERE email = $1 AND phoneNumber = $2;`,[email, phoneNumber]);
+            console.log("2");
             console.log(email,phoneNumber);
             console.log(result3);
             const answer=await send_res(result3);
+             console.log("3");
             console.log("ok",answer);
             res.status(200).json(answer);
         }
         else{
+            //user with same email and phone number exist
           const answer=await send_res(result);
           console.log("ok",answer);
           res.status(200).json(answer);
@@ -98,69 +126,6 @@ app.listen(process.env.APP_PORT,()=>{
 
 /*
 
-
-contacts me nodes--> id,phone,email,linkedid,linkprecedance,created,updated,deleted
-
-
-n1=e1,p1
-
-if(e1 && p1){
-    if(dono ek hii node ke hai){
-        uski linked list return karo
-    }
-    else{
-        dono diff person hai abhi , too first merge
-        jiska primary contact ka smallest creation time hoo vo primary rahega
-        **LINEARITY maintain karne ke liye merge karo 
-        phir uski linked list return karo
-    }
-    
-}
-else if(e1){
-    //secondary banega
-    {contact mai dallo
-    largest id node jiska email same hoo vo iska papa
-    cte se sab mail, phone number and id lelo
-    topmost(ref null hoga) usko specially handle karo kyuki voo first element hoga arrays mai}=jj
-
-}
-else if(p1){
-    //secondary banega
-    {jj}
-}
-else{
-    //primary banega
-    contact mai new data n1
-
-}
-
-
-
-//approach 2
-
-
-customer table (id,email,phone,update time)
-1
-2
-3
-4
-5
-6
-
-user table (id,)
-1 u1
-2 u2
-3 u1
-4 u2
-5 u1
-6 u1
-
-bottom table
-
-u1 6
-u2 4
-
-curl -X POST http://localhost:3000/identify \ -H "Content-Type: application/json" \ -d '{"email":"happy@gmail","phoneNumber":"1234"}'
 
 
 
